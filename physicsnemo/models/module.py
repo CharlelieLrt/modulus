4e8a004a9f1edeb8bf4a7ba21da3ed19f971690d
Charlelie Laurent <claurent@nvidia.com>
2025-07-11 11:42:25 +0800

    Removed unecessary check in args overriding
    Signed-off-by: Charlelie Laurent <claurent@nvidia.com>

--- physicsnemo/models/module.py
+++ physicsnemo/models/module.py
@@ -158,8 +158,8 @@ class Module(torch.nn.Module):
     ) -> None:
         """Safely override ``__init__`` arguments stored in a checkpoint.
 
-        This updates *args* *in-place* with the values provided in
-        *override_args*. Only keys defined in ``cls._overridable_args`` are
+        This updates ``args`` *in-place* with the values provided in
+        ``override_args``. Only keys defined in ``cls._overridable_args`` are
         allowed to be modified. Attempting to override any other key will raise
         a ``ValueError``.
 
@@ -176,10 +176,6 @@ class Module(torch.nn.Module):
             if key not in cls._overridable_args:
                 raise ValueError(
                     f"Argument '{key}' cannot be overridden for " f"{cls.__name__}."
-                )
-            if key not in args:
-                raise ValueError(
-                    f"Unexpected argument '{key}' to override for " f"{cls.__name__}."
                 )
             args[key] = value
 
