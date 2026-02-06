614b15ada3e0b6335acc8d2292e2e3cdd76b7e62
Charlelie Laurent <claurent@nvidia.com>
2026-02-06 13:21:05 -0800

    Initial commit of guidance
    Signed-off-by: Charlelie Laurent <claurent@nvidia.com>

--- physicsnemo/diffusion/guidance/__init__.py
+++ physicsnemo/diffusion/guidance/__init__.py
@@ -13,3 +13,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""DPS (Diffusion Posterior Sampling) guidance for diffusion models."""
+
+from .dps_guidance import DPSDenoiser, DPSGuidance
+
+__all__ = [
+    "DPSGuidance",
+    "DPSDenoiser",
+]
